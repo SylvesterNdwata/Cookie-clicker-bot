6c6718f27355829931d13950b7817a1b549e9508
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-02-14 19:07:20 +0100

    Cookie clicking loop and choosing the highest reward

--- main.py
+++ main.py
@@ -13,21 +13,45 @@ bot.get("http://ozh.github.io/cookieclicker/")
 # cookies = bot.find_element(By.CSS_SELECTOR, value="div.cc_container--open a")
 # cookies.click()
 
+# bot.implicitly_wait(5)
 wait = WebDriverWait(bot, 10)
 
 wait.until(ec.presence_of_element_located((By.ID, "langSelect-EN")))
 lang_select = bot.find_element(By.CSS_SELECTOR, value="div#langSelect-EN")
 lang_select.click()
 
+most_valueable_reward = 0
+most_expensive = None
 x = 0
 last_run = time.time()
-while x < 102:
+while True:
+    
+    if time.time() - last_run >= 300:
+        break
     
     wait.until(ec.presence_of_element_located((By.ID, "bigCookie")))
     cookie_button = bot.find_element(By.ID, value="bigCookie")
     cookie_button.click()
-    x += 1
     
-    # if time.time() - last_run >= 5:
+    if time.time() - last_run >= 5:
+        wait.until(ec.presence_of_element_located((By.ID, "product0")))
+        rewards = bot.find_elements(By.CSS_SELECTOR, value="div.product.unlocked.enabled")
+
+
+        for reward in rewards:
+            # wait.until(ec.presence_of_element_located((By.ID, "priceProduct0")))
+            
+            reward_price = reward.find_element(By.CSS_SELECTOR, value="span.price").get_attribute("innerText").replace(",", "")
+            if int(reward_price) > most_valueable_reward:
+                most_valueable_reward = int(reward_price)
+                
+                most_expensive = reward
+                most_expensive.click()
+                
+                
+                print(most_valueable_reward)
+        last_run = time.time()
+                
+    
         
     
